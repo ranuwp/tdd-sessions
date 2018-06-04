ad55aaadfe783ffd8b05e3593d25a957da5e91a0
Ranu Wijaya Putra <ranu.putra@btpn.com>
2018-06-05 02:32:22 +0700

    [Ranu][T] for register same cashtag

--- test/Bank.test.js
+++ test/Bank.test.js
@@ -12,4 +12,21 @@ describe('Bank', () => {
     const firstMember = members.get(cashtag);
     expect(firstMember).toEqual(ranu);
   });
+
+  it('should throw error "Cashtag has been registered" when register member with same cashtag', () => {
+    const cashtag = '$ranuwp';
+    const ranu = {
+      cashtag,
+    };
+    const mark = {
+      cashtag,
+    };
+    const members = new Map();
+    const bank = new Bank(members);
+    bank.register(ranu);
+    const registerSameCashtag = () => {
+      bank.register(mark);
+    };
+    expect(registerSameCashtag).toThrowError('Cashtag has been registered');
+  });
 });

73890fa6bfe065cfb4d3a97dd7cdbba69c3eece3
Ranu Wijaya Putra <ranu.putra@btpn.com>
2018-06-05 02:39:32 +0700

    [Ranu][S] for register same cashtag

--- src/Bank.js
+++ src/Bank.js
@@ -5,6 +5,14 @@ export default class Bank {
 
   register(newMember) {
     const { cashtag } = newMember;
+    this._validateCashtag(cashtag);
     this.members.set(cashtag, newMember);
+  }
+
+  _validateCashtag(cashtag) {
+    const member = this.members.get(cashtag);
+    if (member) {
+      throw Error('Cashtag has been registered');
+    }
   }
 }
